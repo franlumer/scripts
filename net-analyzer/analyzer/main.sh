bf7a60a3a8f735ef3d8f8f813348631be035971b
franlumer09 <franlumer09@gmail.com>
2026-04-20 09:38:54 -0300

    .

---
+++ net-analyzer/analyzer/main.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# discovery de hosts -> nmap-hosts.xml
+nmap -sn 192.168.50.0/24 -oX ../data/nmap-hosts.xml -vvv
+
+# saca todos los hosts con status "up" -> up-hosts.txt
+xmlstarlet sel -t -m "//host[status/@state='up']/address" -v "@addr" -n ../data/nmap-hosts.xml > ../data/up-hosts.txt
+
+# escaneo los puertos de los hosts -> ports.xml
+sudo nmap -Pn -sSV --top-ports 100 -iL ../data/up-hosts.txt -oX ../data/ports.xml -vvv
+
+python3 xml-analysis.py
+
+# nmap-hosts.xml
+# nmaprun
+# └── host
+#     ├── status (@state="up/down", @reason)
+#     ├── address (@addr="45.33.50.110", @addrtype="ipv4")
+#     ├── address (@addr="...", @addrtype="mac")  <- solo si está disponible
+#     └── hostnames
+#         └── hostname (@name, @type)
+
+# nmaprun
+# └── host
+#     ├── status (@state, @reason)
+#     ├── address (@addr, @addrtype)      <- puede haber ipv4 y mac
+#     ├── hostnames
+#     │   └── hostname (@name, @type)
+#     └── ports
+#         └── port (@protocol, @portid)
+#             ├── state (@state, @reason)
+#             └── service (@name, @product, @version, @method)
+#                 └── cpe                 <- solo cuando nmap identifica el servicio
