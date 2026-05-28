7bc53808e5b47722d398e969a2ed09532648eed4
franlumer <franlumer09@gmail.com>
2026-05-28 11:41:23 -0300

    -

--- whichServer/whichServer.py
+++ whichServer/whichServer.py
@@ -1,13 +1,23 @@
+#!/usr/bin/python3
+
 import sys
+from pathlib import Path
 
 try:
     SEARCH = sys.argv[1] 
 
-    with open ('servers.txt', 'r') as SERVERS:  
+    servers_file = Path.home() / 'scripts' / 'servers.txt'
+    found = False
+    
+    with open (servers_file, 'r') as SERVERS:  
         lines = SERVERS.readlines()
         for line in lines:
-            IP, HOSTNAME = line.split(' - ')
+            IP, HOSTNAME = line.strip().split(' - ')
             if SEARCH == IP:
-                print(f'{SEARCH} -> HOSTNAME')
+                print(f'[+] {SEARCH} -> {HOSTNAME}')
+                found = True
+
+    if not found: 
+        print('[!] Server not found')
 except Exception as e: 
     print(e)
